c0e326218daf3fa74a78904bbe30499ab61238c4
ttiimm2214 <timchen@umich.edu>
2019-03-28 01:29:15 +0800

    add 1_two_sum folder

---
+++ 1_two_sum/1_two_sum.cpp
@@ -0,0 +1,20 @@
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//Example:
+//Given nums = [2, 7, 11, 15], target = 9,
+
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+//test case
+	vector<int> nums;
+	nums.push_back(1);
+	cout<<nums[0]<<endl; 
+}
+
