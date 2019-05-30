026bf837d83188e2020d668be329f0a7103deeca
ttiimm2214 <timchen@umich.edu>
2019-05-30 18:13:03 +0800

    complete 1.0 version

--- 1_two_sum/1_two_sum.cpp
+++ 1_two_sum/1_two_sum.cpp
@@ -19,14 +19,42 @@
 
 #include <iostream>
 #include <vector>
-
+#include <unordered_map>
 using namespace std;
 
 int main(){
-//test case
+//test case num = [2, 7, 11, 15] target = 9
+	vector<int> result;
 	vector<int> nums;
+	unordered_map<int,int> mapping; 
+	int TARGET = 9;
+	int remain;
 	nums.push_back(2);
 	nums.push_back(7);
-	cout<<nums[1]<<endl; 
+	nums.push_back(11);
+	nums.push_back(15);
+	int SIZE = nums.size();
+// solution 
+	for (int i = 0; i < nums.size(); i++){
+		mapping[nums[i]]= i;
+	}
+
+	for(int i = 0; i < nums.size(); i++){
+		remain = TARGET - nums [i];
+		if (mapping.find(remain)!=mapping.end()&& mapping[remain] != i){
+			result.push_back(i);
+			result.push_back(mapping[remain]);
+			break;
+		}
+		
+	}
+	
+// output for leetcode 
+	// return result;
+// output 
+	for (int i = 0; i < result.size(); i++){
+		cout<< result [i]<< endl;
+	}
+
 }
 
