e11aee15086c8eaeb2c66f0bf03c5591f6b0d84b
ttiimm2214 <timchen@umich.edu>
2019-06-20 18:09:39 +0800

    L21 init

---
+++ 21_Merge_Two_Sorted_Lists/21_M_T_S_L.cpp
@@ -0,0 +1,22 @@
+/*
+ * Leetcode 21_Merge_Two_Sorted_Lists`
+ *
+ * Compile: g++ 21_M_T_S_L.cpp -o result
+ * Execute: ./result
+ */
+
+
+/*
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+Example:
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+}
